127639fed304a7da378f61b09b0806f276549cd9
NebulaBC <thecheeseburgerfactory32@gmail.com>
2021-07-06 17:00:48 -0400

    Add files via upload

--- src/main/java/net/nebulabc/autorestart/Autorestart.java
+++ src/main/java/net/nebulabc/autorestart/Autorestart.java
@@ -1,7 +1,9 @@
 package net.nebulabc.autorestart;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
@@ -17,16 +19,24 @@ public final class Autorestart extends JavaPlugin {
         this.getConfig();
         FileConfiguration config = this.getConfig();
         config.addDefault("hour", 24);
+        config.addDefault("prefix", "Autorestart");
+        config.addDefault("maincolor", "§c");
+        config.addDefault("secondarycolor", "§8");
+        config.addDefault("textcolor", "§c");
         config.options().copyDefaults(true);
         saveConfig();
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println("Hypatios Autorestart has initialized");
+        System.out.println("Autorestart has initialized");
         System.out.println("Copyright 2021 NebulaBC All rights reserved.");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("[Autorestart] Your restart time is set to " + config.get("hour"));
 
         int hourvar = (config.getInt("hour"));
         int minutevar = (config.getInt("minute"));
+        String prefix = (config.getString("prefix"));
+        String maincolor = (config.getString("maincolor"));
+        String secondarycolor = (config.getString("secondarycolor"));
+        String textcolor = (config.getString("textcolor"));
         Plugin plugin = this;
         
         BukkitScheduler scheduler = getServer().getScheduler();
@@ -35,20 +45,47 @@ public final class Autorestart extends JavaPlugin {
             public void run()
             {
                 System.out.println("[Autorestart] Restart process started");
-                Bukkit.broadcastMessage("§8[§5Hypatios§8]§5 Restarting in 5 minutes");
+                Bukkit.broadcastMessage(secondarycolor + "[" + maincolor + prefix + secondarycolor + "]" + textcolor + " Restarting in 5 minutes");
                 Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                    Bukkit.broadcastMessage("§8[§5Hypatios§8]§5 Restarting in 3 minutes");
+                    Bukkit.broadcastMessage(secondarycolor + "[" + maincolor + prefix + secondarycolor + "]" + textcolor + " Restarting in 3 minutes");
                         Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                            Bukkit.broadcastMessage("§8[§5Hypatios§8]§5 Restarting in 2 minutes");
+                            Bukkit.broadcastMessage(secondarycolor + "[" + maincolor + prefix + secondarycolor + "]" + textcolor + " Restarting in 2 minutes");
                                 Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                                    Bukkit.broadcastMessage("§8[§5Hypatios§8]§5 Restarting in 1 minutes");
+                                    Bukkit.broadcastMessage(secondarycolor + "[" + maincolor + prefix + secondarycolor + "]" + textcolor + " Restarting in 1 minute");
                                         Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                                            Bukkit.broadcastMessage("§8[§5Hypatios§8]§5 Restarting now");
-                                                Bukkit.spigot().restart();
-                            }, 1200L);
+                                            Bukkit.broadcastMessage(secondarycolor + "[" + maincolor + prefix + secondarycolor + "]" + textcolor + " Restarting in 3");
+                                            for (Player player : Bukkit.getOnlinePlayers())
+                                            {
+                                                player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 100, 0);
+                                            }
+                                                Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                                                    Bukkit.broadcastMessage(secondarycolor + "[" + maincolor + prefix + secondarycolor + "]" + textcolor + " Restarting in 2");
+                                                    for (Player player : Bukkit.getOnlinePlayers())
+                                                    {
+                                                        player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 100, 0);
+                                                    }
+                                                        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                                                            Bukkit.broadcastMessage(secondarycolor + "[" + maincolor + prefix + secondarycolor + "]" + textcolor + " Restarting in 1");
+                                                            for (Player player : Bukkit.getOnlinePlayers())
+                                                            {
+                                                                player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 100, 0);
+                                                            }
+                                                                Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                                                                    Bukkit.broadcastMessage(secondarycolor + "[" + maincolor + prefix + secondarycolor + "]" + textcolor + " Restarting now");
+                                                                    for (Player player : Bukkit.getOnlinePlayers())
+                                                                    {
+                                                                        player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 100, 1);
+                                                                    }
+                                                                        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                                                                            Bukkit.spigot().restart();
+                                                }, 20L);
+                                            }, 20L);
+                                        }, 20L);
+                                    }, 20L);
+                                }, 1160L);
+                            }, 1200);
                         }, 1200L);
-                    }, 1200L);
-                }, 2400L);
+                    }, 2400L);
             }
         }, hourvar);
     }

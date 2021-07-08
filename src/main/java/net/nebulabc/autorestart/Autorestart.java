2123fac2b533c64c36df7ed1a36179aaedff73cf
NebulaBC <thecheeseburgerfactory32@gmail.com>
2021-07-07 20:33:15 -0400

    Add files via upload

--- src/main/java/net/nebulabc/autorestart/Autorestart.java
+++ src/main/java/net/nebulabc/autorestart/Autorestart.java
@@ -8,8 +8,14 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Calendar;
-import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.logging.Logger;
 
 public final class Autorestart extends JavaPlugin {
 
@@ -18,22 +24,17 @@ public final class Autorestart extends JavaPlugin {
         this.saveDefaultConfig();
         this.getConfig();
         FileConfiguration config = this.getConfig();
-        config.addDefault("hour", 24);
-        config.addDefault("prefix", "Autorestart");
-        config.addDefault("maincolor", "§c");
-        config.addDefault("secondarycolor", "§8");
-        config.addDefault("textcolor", "§c");
-        config.options().copyDefaults(true);
         saveConfig();
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println("Autorestart has initialized");
-        System.out.println("Copyright 2021 NebulaBC All rights reserved.");
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("------------------------------------------------");
+        System.out.println("          Autorestart has initialized");
+        System.out.println("  Copyright 2021 NebulaBC All rights reserved.");
+        System.out.println("------------------------------------------------");
         System.out.println("[Autorestart] Your restart time is set to " + config.get("hour"));
 
         int hourvar = (config.getInt("hour"));
-        int minutevar = (config.getInt("minute"));
         String prefix = (config.getString("prefix"));
+        String title = (config.getString("title"));
+        String sound = (config.getString("sound"));
         String maincolor = (config.getString("maincolor"));
         String secondarycolor = (config.getString("secondarycolor"));
         String textcolor = (config.getString("textcolor"));
@@ -56,25 +57,45 @@ public final class Autorestart extends JavaPlugin {
                                             Bukkit.broadcastMessage(secondarycolor + "[" + maincolor + prefix + secondarycolor + "]" + textcolor + " Restarting in 3");
                                             for (Player player : Bukkit.getOnlinePlayers())
                                             {
-                                                player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 100, 0);
+                                                if(sound.equals("true")) {
+                                                    player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 100, 0);
+                                                }
+                                                if(title.equals("true")){
+                                                    player.sendTitle("Restarting in 3", "", 0, 1, 0);
+                                                }
                                             }
                                                 Bukkit.getScheduler().runTaskLater(plugin, () -> {
                                                     Bukkit.broadcastMessage(secondarycolor + "[" + maincolor + prefix + secondarycolor + "]" + textcolor + " Restarting in 2");
                                                     for (Player player : Bukkit.getOnlinePlayers())
                                                     {
-                                                        player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 100, 0);
+                                                        if(sound.equals("true")) {
+                                                            player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 100, 0);
+                                                        }
+                                                        if(title.equals("true")){
+                                                            player.sendTitle("Restarting in 2", "", 0, 1, 0);
+                                                        }
                                                     }
                                                         Bukkit.getScheduler().runTaskLater(plugin, () -> {
                                                             Bukkit.broadcastMessage(secondarycolor + "[" + maincolor + prefix + secondarycolor + "]" + textcolor + " Restarting in 1");
                                                             for (Player player : Bukkit.getOnlinePlayers())
                                                             {
-                                                                player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 100, 0);
+                                                                if(sound.equals("true")) {
+                                                                    player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 100, 0);
+                                                                }
+                                                                if(title.equals("true")){
+                                                                    player.sendTitle("Restarting in 1", "", 0, 1, 0);
+                                                                }
                                                             }
                                                                 Bukkit.getScheduler().runTaskLater(plugin, () -> {
                                                                     Bukkit.broadcastMessage(secondarycolor + "[" + maincolor + prefix + secondarycolor + "]" + textcolor + " Restarting now");
                                                                     for (Player player : Bukkit.getOnlinePlayers())
                                                                     {
-                                                                        player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 100, 1);
+                                                                        if(sound.equals("true")) {
+                                                                            player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 100, 0.2F);
+                                                                        }
+                                                                        if(title.equals("true")){
+                                                                            player.sendTitle("Restarting now", "", 0, 1, 0);
+                                                                        }
                                                                     }
                                                                         Bukkit.getScheduler().runTaskLater(plugin, () -> {
                                                                             Bukkit.spigot().restart();
@@ -88,6 +109,17 @@ public final class Autorestart extends JavaPlugin {
                     }, 2400L);
             }
         }, hourvar);
+
+        Logger logger = this.getLogger();
+
+        new UpdateChecker(this, 94004).getVersion(version -> {
+            if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
+                logger.info("There is not a new update available.");
+            } else {
+                logger.info("There is a new update available.");
+            }
+        });
+
     }
 
     @Override
